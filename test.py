5dc121fa6f2dd15bc92417bcfea0f7964792ade1
adavoodk <78701487+adavoodk@users.noreply.github.com>
2021-02-08 00:42:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,20 +4,21 @@ import ldclient
 if __name__ == "__main__":
   ldclient.set_config(Config("sdk-500579db-59ab-45a8-b120-27d37b5d639c"))
 
-  user = {
-    "key": "bob@example.com",
-    "firstName": "Bob",
-    "lastName": "Loblaw",
-    "custom": {
-      "groups": "beta_testers"
-    }
+user = {
+  "key": "UNIQUE IDENTIFIER",
+  "firstName": "Bob",
+  "lastName": "Loblaw",
+  "custom": {
+    "groups": "beta_testers"
   }
+}
 
-  show_feature = ldclient.get().variation("YOUR_FLAG_KEY", user, False)
+show_feature = ldclient.get().variation("flag", user, False)
 
-  if show_feature:
-    print "Showing your feature"
-  else:
-    print "Not showing your feature"
+if show_feature:
+  print("Showing your feature")
+else:
+  print("Not showing your feature")
 
-  ldclient.get().flush()
+ldclient.get().close()
+
